1402ea004d6a7895455b70f151dcb2702686eae1
Hamidov6611 <dilmuroddelitot@gmail.com>
2023-09-04 10:48:49 +0500

    Home Category

--- src/page/HomeCategory.jsx
+++ src/page/HomeCategory.jsx
@@ -10,7 +10,7 @@ const HomeCategory = () => {
     try {
       const { data } = await axios.get(`${url}/category_all_views/`);
       setCategory(data);
-      console.log(data);
+      console.log(data)
     } catch (error) {
       console.log(error);
     }
@@ -19,6 +19,22 @@ const HomeCategory = () => {
   useEffect(() => {
     categoryHandler();
   }, []);
+
+  let widthCard = [{left: 30, right: 68}, {left: 58, right: 40}, {left: 38, right: 60}];
+  const shuffleBg = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    widthCard = array;
+    return array;
+  };
+  useEffect(() => {
+    shuffleBg(widthCard)
+    console.log(widthCard)
+  }, [])
 
   return (
     <div className="flex flex-col mb-4 p-1 sm:p-4 md:w-[96%] mx-auto mt-[40px]">
@@ -37,19 +53,20 @@ const HomeCategory = () => {
                 className="w-[100%] h-[100%] object-cover"
               />
             </div>
-            <div className="absolute top-0 left-0 bg-modal w-[100%]">
+            <div className="absolute top-0 left-0 bg-modal3 w-[100%]">
               <p
-                className="py-3 text-[34px] font-semibold text-white text-center"
-                style={{ letterSpacing: "5px" }}
+              style={{textTransform:"capitalize", letterSpacing: '5px'}}
+                className="py-3 text-[18px] font-medium capitalize sm:text-[24px] md:text-[34px] md:font-semibold text-white text-center"
+            
               >
-                {item?.title}
+                {item?.title.toLowerCase()}
               </p>
             </div>
           </Link>
         ))}
       </div>
       <div className="w-[100%] flex justify-center">
-      <Link to={'/букеты'} className="py-[20px] px-[60px] text-[12px] lg:text-[20px] font-montserrat rounded-lg text-[#fff] bg-[#443926]">
+      <Link to={'/букеты'} className="py-[20px] px-[60px] text-[12px] lg:text-[20px] font-montserrat rounded-lg text-[#fff] bg-[#C59A77]">
         Смотреть все разделы
       </Link>
       </div>
