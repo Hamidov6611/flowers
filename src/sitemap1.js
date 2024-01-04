56bea7872cd36e63a0bb1d558540f5f4200c8852
Hamidov6611 <dilmuroddelitot@gmail.com>
2024-01-04 13:46:48 +0500

    update xml

--- src/sitemap1.js
+++ src/sitemap1.js
@@ -14,9 +14,9 @@ const Sitemap = () => {
     );
 
     return `<?xml version="1.0" encoding="UTF-8"?>
-      <user xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${sitemapItems.join("")}
-      </user>`;
+      </urlset>`;
   };
 
   // Рендеринг компонента
